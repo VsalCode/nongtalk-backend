50b183add6adb5bb6918c1648c211a8d06cf0b87
VsalCode <vsal.ilham@gmail.com>
2025-07-29 03:18:59 +0700

    feat(auth): add login endpoint

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,30 +1,84 @@
-import { Response,  Request } from "express"
+import { Response, Request } from "express"
 import { prisma } from "../db/config"
 import { ApiResponse } from "../utils/response";
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 export const register = async (req: Request, res: Response<ApiResponse>) => {
-  const { email, username, password } = req.body;
+  try {
+    const { email, username, password } = req.body;
 
-  const existing = await prisma.user.findUnique({ where: { email } });
-  if (existing) return res.status(400).json({
-    success: false,
-    message: "Email already used"
-  });
+    const existing = await prisma.user.findUnique({ where: { email } });
+    if (existing) {
+      res.status(400).json({
+        success: false,
+        message: "Email already used"
+      });
+      return
+    }
 
-  const hash = await bcrypt.hash(password, 10);
-  const user = await prisma.user.create({
-    data: {
-      email,
-      username,
-      userId: "USR" + Math.floor(100000 + Math.random() * 900000),
-      password: hash,
-    },
-  });
+    const hash = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: {
+        email,
+        username,
+        userId: "USR" + Math.floor(100000 + Math.random() * 900000),
+        password: hash,
+      },
+    });
 
-  res.status(201).json({
-    success: true,
-    message: "user registered successfully!",
-    results: user
-  });
+    res.status(201).json({
+      success: true,
+      message: "user registered successfully!",
+      results: user
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to send request!",
+      errors: err
+    });
+  }
+
 };
+
+export const Login = async (req: Request, res: Response<ApiResponse>) => {
+  try {
+    const { email, password } = req.body
+
+    const userLogin = await prisma.user.findUnique({ where: { email } })
+    if (!userLogin) {
+      res.status(400).json({
+        success: false,
+        message: "User Not Found"
+      });
+      return
+    }
+
+    const isMatch = await bcrypt.compare(password, userLogin.password );
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials",
+        errors: { password: "Incorrect password" },
+      });
+    }
+
+    const secret = process.env.JWT_SECRET
+    const token = jwt.sign({ id: userLogin.id }, secret , { expiresIn: "1d" });
+
+    res.status(200).json({
+      success: true,
+      message: "user login successfully!",
+      results: token
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to send request!",
+      errors: err
+    });
+  }
+}
--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -1,7 +1,8 @@
 import { Router } from "express"
-import { register } from "../controllers/auth.controller"
+import { Login, register } from "../controllers/auth.controller"
 const router  = Router()
 
 router.post("/register", register)
+router.post("/login", Login)
 
 export default router
